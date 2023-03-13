b235be97b798e0ee01452be29bef26c8650f7e46
StiltonBree <nruth@g.clemson.edu>
2023-03-13 12:36:02 -0700

    sql

---
+++ customers.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE customers
+
+/* Database of Customers, Tables of Traits*/
+GO
+
+    CREATE TABLE hair();
+
+    CREATE TABLE height();
+
+    CREATE TABLE holiday();
+
+    CREATE TABLE debut();
